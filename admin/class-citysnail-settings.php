b6a561267a088bc54ee8e93bd0c83a16fa7d5c2c
Joseph Scoggins <borninthecaul@gmail.com>
2020-03-19 00:54:58 -0700

    admin pages

--- admin/class-citysnail-settings.php
+++ admin/class-citysnail-settings.php
@@ -4,20 +4,11 @@ class Citysnail_Settings {
 
   public static $field_count = 5;
   public static $eq_label_toggle = array(
-    "param",
-    "shortcode",
-    "fallback"
+    "url",
+    "primary_keyword",
+    "secondary_keyword"
   );
-  public static $geo_label_toggle = array(
-    "phone",
-    "phone_shortcode",
-    "locale",
-    "locale_shortcode",
-    "region",
-    "region_shortcode",
-    "service_area",
-    "service_area_shortcode"
-  );
+
   public static $current_field_index = 0;
   public static $eq_label_toggle_index = 0;
   public static $geo_label_toggle_index = 0;
@@ -37,6 +28,22 @@ class Citysnail_Settings {
       'wp_citysnail_keywords'                         //page-slug
     );
 
+    add_settings_field(
+      'domain',                   //uniqueID - "param_1", etc.
+      'Domain',                  //uniqueTitle -
+      array('Citysnail_Settings','wp_citysnail_domain_field'),//callback
+      'wp_citysnail',                   //page-slug
+      'wp_citysnail_settings'          //section (parent settings-section uniqueID)
+    );
+
+    add_settings_field(
+      'sitemap',                   //uniqueID - "param_1", etc.
+      'Sitemap',                  //uniqueTitle -
+      array('Citysnail_Settings','wp_citysnail_sitemap_field'),//callback
+      'wp_citysnail',                   //page-slug
+      'wp_citysnail_settings'          //section (parent settings-section uniqueID)
+    );
+
     for ($i = 1; $i < self::$field_count + 1; $i++) {
       self::$current_field_index = $i;
       for ($ii = 0; $ii < count(self::$eq_label_toggle); $ii++) {
@@ -47,26 +54,13 @@ class Citysnail_Settings {
         add_settings_field(
           $this_field,                   //uniqueID - "param_1", etc.
           $this_label,                  //uniqueTitle -
-          array('Citysnail_Settings','wp_citysnail_settings_field'),//callback cb_equips_settings_field();
-          'wp_citysnail',                   //page-slug
-          'wp_citysnail_settings'          //section (parent settings-section uniqueID)
+          array('Citysnail_Settings','wp_citysnail_keywords_field'),//callback
+          'wp_citysnail_keywords',                   //page-slug
+          'wp_citysnail_keywords'          //section (parent settings-section uniqueID)
         );
       }
     }
     self::$current_field_index = 1;
-    for ($iii = 0; $iii < count(self::$geo_label_toggle); $iii++) {
-      $geo_field_name = self::$geo_label_toggle[$iii];
-      $this_geo_field = $geo_field_name;
-      $this_geo_label = ucwords(str_replace("_", " ", $geo_field_name));
-
-      add_settings_field(
-        $this_geo_field,                   //uniqueID - "param_1", etc.
-        $this_geo_label,                  //uniqueTitle -
-        array('Citysnail_Settings','wp_citysnail_keywords_field'),//callback cb_equips_settings_field();
-        'wp_citysnail_keywords',                   //page-slug
-        'wp_citysnail_keywords'          //section (parent settings-section uniqueID)
-      );
-    }
 
     register_setting( 'wp_citysnail', 'wp_citysnail' );
     register_setting( 'wp_citysnail_keywords', 'wp_citysnail_keywords' );
@@ -76,7 +70,7 @@ class Citysnail_Settings {
 
   ////template 3 - settings section field - dynamically rendered <input/>
 
-  static function wp_citysnail_settings_field() {
+  static function wp_citysnail_keywords_field() {
     $options = get_option('wp_citysnail');
     //error_log(print_r($options));
     //local namespace assignments based on global settings &/or database state
@@ -94,27 +88,25 @@ class Citysnail_Settings {
     self::$current_field_index += (self::$eq_label_toggle_index === 0) ?
       1 : 0;
     //make an <input/> with dynamic attributes
-    echo "<input type='text' name=wp_citysnail[{$this_field}] {$value_tag}='{$placeholder}'/>" . $divider;
-  }
-
-  static function wp_citysnail_keywords_field() {
-    $options = get_option('wp_citysnail_keywords');
-    //error_log(print_r($options));
-    //local namespace assignments based on global settings &/or database state
-    $divider = (self::$geo_label_toggle_index < count(self::$geo_label_toggle)-1) ?
-      "" : "<br/><br/><hr/>";
-    $field_name = self::$geo_label_toggle[self::$geo_label_toggle_index];
-    $this_field = $field_name;
-    $this_label = ucwords($field_name);
-    $placeholder = ("" != ($options[$this_field])) ? $options[$this_field] : "(not set)";
-    $value_tag = ($placeholder === "(not set)") ? "placeholder" : "value";
-    //reset globals - toggle label and increment pairing series as needed
-    self::$geo_label_toggle_index +=
-      (self::$geo_label_toggle_index < count(self::$geo_label_toggle)-1 ) ?
-      1 : -(count(self::$geo_label_toggle)-1);
-    //make an <input/> with dynamic attributes
     echo "<input type='text' name=wp_citysnail_keywords[{$this_field}] {$value_tag}='{$placeholder}'/>" . $divider;
   }
+
+  static function wp_citysnail_domain_field() {
+    $options = get_option('wp_citysnail');
+    $this_field = 'domain';
+    $placeholder = ("" != ($options[$this_field])) ? $options[$this_field] : "(myplace.net)";
+    $value_tag = ($placeholder === "(myplace.net)") ? "placeholder" : "value";
+    echo "<input type='text' name=wp_citysnail[$this_field] {$value_tag}='{$placeholder}'/>";
+  }
+
+  static function wp_citysnail_sitemap_field() {
+    $options = get_option('wp_citysnail');
+    $this_field = 'sitemap';
+    $placeholder = ("" != ($options[$this_field])) ? $options[$this_field] : "(page-sitemap.xml)";
+    $value_tag = ($placeholder === "(page-sitemap.xml)") ? "placeholder" : "value";
+    echo "<input type='text' name=wp_citysnail[$this_field] {$value_tag}='{$placeholder}'/>";
+  }
+
 
   ////template 2 - after settings section title
 
